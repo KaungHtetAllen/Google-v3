7ec260ccc095ba5f79aae9504f7f59f080c88192
KaungHtetAllen <kaunghtet22122003@gmail.com>
2022-08-07 17:37:56 +0630

    create body section

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import Head from 'next/head'
-import Header from '../components/Header'
-
+import Head from 'next/head';
+import Header from '../components/Header';
+import Image from 'next/image';
+import {SearchIcon, MicrophoneIcon} from "@heroicons/react/solid";
 
 export default function Home() {
   return (
@@ -15,6 +16,23 @@ export default function Home() {
       <Header/>
 
       {/* Body */}
+      <form className='flex flex-col items-center mt-40'>
+        <Image 
+        width="300"
+        height="100"
+        objectFit='cover'
+        src="https://www.google.com.mm/images/branding/googlelogo/2x/googlelogo_color_160x56dp.png"
+        />
+        <div className='flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 hover:shadow-lg focus-within:shadow-lg px-5 py-3 rounded-full items-center sm:max-w-xl lg:max-w-2xl'>
+          <SearchIcon className='h-5 text-gray-500 mr-3'/>
+          <input type="text" className='flex-grow focus:outline-none '/>
+          <MicrophoneIcon className="h-5"/>
+          </div>
+          <div className='flex flex-col sm:flex-row w-[50%] space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center'>
+          <button className='btn'>Google Search</button>
+          <button className='btn'>I'm Feeling Lucky</button>
+          </div>
+      </form>
 
       {/* Footer */}
     </div>
