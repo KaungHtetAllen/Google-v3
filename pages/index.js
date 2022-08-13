a60538279afb3412ebf1c846513020152f80eb95
KaungHtetAllen <kaunghtet22122003@gmail.com>
2022-08-13 17:51:09 +0630

    fix the remaining buttons plus Im feeling lucky button as well as adding the secret

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,13 @@ export default function Home() {
     router.push(`/search?term=${term.trim()}&searchType=`)
   }
 
+  async function randomSearch(event){
+    event.preventDefault();
+    const randomTerm = await fetch("https://random-word-api.herokuapp.com/word?number=1").then((response)=>response.json());
+    if(!randomTerm)return;
+    router.push(`/search?term=${randomTerm}&searchType=`)
+  }
+
   return (
     <div>
       <Head>
@@ -43,7 +50,7 @@ export default function Home() {
           </div>
           <div className='flex flex-col sm:flex-row w-[50%] space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center'>
           <button onClick={search} className='btn'>Google Search</button>
-          <button className='btn'>I'm Feeling Lucky</button>
+          <button onClick={randomSearch} className='btn'>I&apos;m Feeling Lucky</button>
           </div>
       </form>
 
