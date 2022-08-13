8c8f990bf41700d78b3939cb6051781617362cb6
KaungHtetAllen <kaunghtet22122003@gmail.com>
2022-08-13 21:20:48 +0630

    finding error

--- pages/search.js
+++ pages/search.js
@@ -6,7 +6,7 @@ import { useRouter } from "next/router";
 import ImageResults from "../components/imageResults";
 
 
-export default function Search({results}){
+export default function search({results}){
     console.log(results);
     const router = useRouter();
     return(
