4636027405a34f2b7104baf643c2ec545a52fa11
KaungHtetAllen <109457000+KaungHtetAllen@users.noreply.github.com>
2022-08-18 21:44:26 +0630

    Update SearchResults.js

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -5,7 +5,7 @@ import PaginationButtons from "./PaginationButtons";
 export default function SearchResults({results}){
     return(
         <div className="w-full mx-auto px-3 sm:pl-[5%] md:pl-[14%] lg:pl-52">
-            <p className="text-gray-600 text-sm mb-5 mt-3">About {results.searchInformation.formattedTotalResults} results ({results.searchInformation.formattedSearchTime} seconds)</p>
+            <p className="text-gray-600 text-sm mb-5 mt-3">About {results.searchInformation.totalResults} results in ({results.searchInformation.formattedSearchTime} seconds)</p>
             {results.items?.map((result)=>(
                 <div key={result.link} className="max-w-xl mb-8">
                     <div className="group">
